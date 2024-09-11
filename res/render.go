5368ee1ea1d4e6e0de81e89b257261a7fa31c5ca
Tanmay Mishu <tanmaymishu@gmail.com>
2024-09-12 01:05:31 +0600

    WIP

--- res/render.go
+++ res/render.go
@@ -5,9 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/lemmego/api/shared"
 )
@@ -40,67 +38,48 @@ func init() {
 	}
 }
 
+func createTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// Find all .tmpl files (including in nested directories)
+	templates, err := filepath.Glob("./templates/**/*.tmpl")
+	if err != nil {
+		return myCache, fmt.Errorf("error finding templates: %v", err)
+	}
+
+	// Loop through all the template files
+	for _, tmpl := range templates {
+		// Extract the file name (e.g., bar.tmpl)
+		name := filepath.Base(tmpl)
+
+		// Create a new template set for each file
+		ts := template.New(name)
+
+		// Parse all templates into the same template set (including the current template)
+		ts, err = ts.ParseFiles(templates...)
+		if err != nil {
+			return myCache, fmt.Errorf("error parsing templates for %s: %v", tmpl, err)
+		}
+
+		// Store the parsed template set in the cache, using the relative path of the template
+		myCache[tmpl] = ts
+	}
+
+	return myCache, nil
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string, data *TemplateData) error {
+	// Look up the requested template from the cache
 	t, ok := templateCache[tmpl]
 	if !ok {
 		return fmt.Errorf("template %s not found in cache", tmpl)
 	}
 
-	// Apply FuncMap here
+	// Apply the FuncMap if provided
 	if data.FuncMap != nil {
 		t = t.Funcs(data.FuncMap)
 	}
 
+	// Execute the template with the provided data
 	return t.Execute(w, data)
-}
-
-func createTemplateCache() (map[string]*template.Template, error) {
-	myCache := map[string]*template.Template{}
-
-	// Find all layout templates (glob pattern for nested directories)
-	layouts, err := filepath.Glob("./templates/**/*.layout.tmpl")
-	if err != nil {
-		return myCache, fmt.Errorf("error finding layout templates: %v", err)
-	}
-
-	// Walk through the template directory to find all .page.tmpl files
-	err = filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Skip directories, only process .tmpl files
-		if !info.IsDir() && filepath.Ext(path) == ".tmpl" && strings.HasSuffix(path, ".page.tmpl") {
-			relPath, err := filepath.Rel("./templates", path) // Get relative path from ./templates
-			if err != nil {
-				return err
-			}
-
-			// Create a new template set and parse the page template
-			ts := template.New(filepath.Base(path))
-			ts, err = ts.ParseFiles(path)
-			if err != nil {
-				return fmt.Errorf("error parsing template %s: %v", relPath, err)
-			}
-
-			// Parse all found layout templates into the same template set
-			if len(layouts) > 0 {
-				ts, err = ts.ParseFiles(layouts...)
-				if err != nil {
-					return fmt.Errorf("error parsing layout templates for %s: %v", relPath, err)
-				}
-			}
-
-			// Cache the template using the relative path (e.g., "foo/bar.page.tmpl")
-			myCache[relPath] = ts
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return myCache, fmt.Errorf("error walking template directory: %v", err)
-	}
-
-	return myCache, nil
 }
